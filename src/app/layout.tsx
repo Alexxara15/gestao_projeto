625215db4c6b7cf0a0be0a2b35401565c84388a1
Alexsandro <alexsandro.araujo015@gmail.com>
2026-01-29 00:42:21 -0300

    feat: Cria o layout raiz da aplicação com barra lateral e cabeçalho.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,11 @@ export default function RootLayout({
           <div className="hidden h-full md:flex md:w-60 md:flex-col md:fixed md:inset-y-0 z-[80] bg-slate-900">
             <Sidebar />
           </div>
-          <main className="md:pl-60 h-full bg-slate-50 min-h-screen">
-            <Header />
-            <div className="p-8 h-[calc(100vh-4rem)] overflow-y-auto">
+          <main className="md:pl-60 h-full bg-slate-50 min-h-screen flex flex-col">
+            <div className="md:hidden">
+              <Header />
+            </div>
+            <div className="p-8 flex-1 overflow-y-auto">
               {children}
             </div>
           </main>
